25792341e1453093efd0f5c474bc596169178fcd
ayakawamura <ayakawamura.b@gmail.com>
2023-01-21 19:12:29 +0900

    homescontroller修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
   def top
-    @posts = Post.limit(3).order(created_at: :desc)
+    # @posts = Post.limit(3).order(created_at: :desc)
   end
 
   def about; end
